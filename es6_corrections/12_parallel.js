0a7a72780bbbb4a716ba4cdde586c9eaa22644b6
gganster <guilian.ganster@gmail.com>
2025-02-07 15:43:40 +0100

    supports & correction es6

---
+++ es6_corrections/12_parallel.js
@@ -0,0 +1,28 @@
+/**
+ * Utilisez la fonction Promise.all pour exécuter 3 appels API en parallèle
+ * 
+ * urls:
+ * - https://jsonplaceholder.typicode.com/todos/1
+ * - https://jsonplaceholder.typicode.com/todos/2
+ * - https://jsonplaceholder.typicode.com/todos/3
+ * 
+ * Retournez un tableau contenant les 3 résultats
+ * 
+ * Vous pouvez utiliser votre fonction apiResponse créé précedement: const {apiResponse} = require("./11_async");
+ * 
+ * documentation: https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
+*/
+
+const {apiResponse} = require("./11_async");
+
+const parallel = async () => {
+    const urls = [
+        "https://jsonplaceholder.typicode.com/todos/1",
+        "https://jsonplaceholder.typicode.com/todos/2",
+        "https://jsonplaceholder.typicode.com/todos/3"
+    ];
+    const promises = urls.map(url => apiResponse(url));
+    return Promise.all(promises);
+}
+
+module.exports = {parallel};
---
+++ es6_corrections/13_bonus.js
@@ -0,0 +1,65 @@
+/**
+
+Exercice : Trouver la fréquence des mots dans une phrase
+
+Objectif :
+Écrire une fonction qui prend en entrée une chaîne de caractères et qui retourne un objet où les clés sont les mots de la phrase et les valeurs sont le nombre de fois où ces mots apparaissent.
+
+Consignes :
+
+Convertir la phrase en tableau de mots.
+Utiliser la méthode .reduce() pour compter la fréquence des mots.
+Retourner l'objet avec les fréquences.
+
+ex: la pomme est rouge et la banane est jaune
+{
+  "la": 2,
+  "pomme": 1,
+  "est": 2,
+  "rouge": 1,
+  "et": 1,
+  "banane": 1,
+  "jaune": 1
+}
+
+ */
+
+const countWords = (sentence) => {
+    const words = sentence.split(" ");
+    if (words.length === 1 && words[0] === "") return {};
+
+    return words.reduce((acc, word) => {
+        acc[word] = acc[word] ? acc[word] + 1 : 1;
+        return acc;
+    }, {});
+};
+
+/**
+Exercice : Trier un objet par valeur
+
+Objectif :
+Écrire une fonction qui prend en entrée un objet et qui retourne un nouvel objet dont les propriétés sont triées en fonction de leurs valeurs, en ordre croissant.
+
+exemple:
+{
+  "pommes": 3,
+  "bananes": 1,
+  "cerises": 8
+};
+
+-> 
+
+{
+  "bananes": 1,
+  "pommes": 3,
+  "cerises": 8
+}
+
+ */
+
+const sortObjectByValue = (obj) => {
+  const entreesTriees = Object.entries(obj).sort((a, b) => a[1] - b[1]);
+  return Object.fromEntries(entreesTriees);
+};
+
+module.exports = {countWords, sortObjectByValue};
